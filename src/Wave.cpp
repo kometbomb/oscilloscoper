f0523b78fb912a8555933228d5fd627f42e6bd38
Tero Lindeman <kometbomb@kometbomb.net>
2018-02-14 19:06:53 +0200

    Skip all other chunks than "data"

--- src/Wave.cpp
+++ src/Wave.cpp
@@ -11,7 +11,7 @@ enum
 
 Wave::Wave()
 	: mSamples(NULL), mLength(0), mSampleRate(44100)
-{	
+{
 }
 
 
@@ -25,15 +25,15 @@ Wave::~Wave()
 bool Wave::load(const char *filename)
 {
 	FILE *f = fopen(filename, "rb");
-	
+
 	if (!f)
 		return false;
-	
-	struct { 
+
+	struct {
 		Chunk c;
 		char WAVEID[4];
 	}  __attribute__((__packed__)) RIFF;
-	
+
 	struct
 	{
 		Chunk c;
@@ -50,69 +50,68 @@ bool Wave::load(const char *filename)
 		long dwChannelMask;
 		char SubFormat[16];
 	} __attribute__((__packed__)) WAVE;
-	
+
 	if (fread(&RIFF, 1 , sizeof(RIFF), f) != sizeof(RIFF) || strncmp(RIFF.c.ckID, "RIFF", 4) != 0 || strncmp(RIFF.WAVEID, "WAVE", 4) != 0)
 	{
 		fclose(f);
 		return false;
 	}
-	
+
 	size_t beginning_of_WAVE = ftell(f);
-	
-	if (fread(&WAVE, 1 , sizeof(WAVE), f) < 16 || strncmp(WAVE.c.ckID, "fmt ", 4) != 0) 
+
+	if (fread(&WAVE, 1 , sizeof(WAVE), f) < 16 || strncmp(WAVE.c.ckID, "fmt ", 4) != 0)
 	{
 		fclose(f);
 		return false;
 	}
-	
+
 	if (WAVE.wFormatTag != WAVE_FORMAT_PCM/* && WAVE.wFormatTag != WAVE_FORMAT_FLOAT*/)
 	{
 		fclose(f);
 		return false;
 	}
-	
+
 	fseek(f, beginning_of_WAVE + WAVE.c.cksize + 8, SEEK_SET);
-	
+
 	Chunk peek = { "", 0 };
-	
-	fread(&peek, 1, sizeof(peek) ,f);
-	
-	if (strncmp(peek.ckID, "fact", 4) == 0)
+
+	// Find "data" chunk while skipping all other types
+
+	do
 	{
-		fseek(f, sizeof(long), SEEK_CUR);
+		int beginningOfChunk = ftell(f);
+		if (fread(&peek, 1, sizeof(peek), f) != sizeof(peek))
+		{
+			fclose(f);
+			return false;
+		}
+
+		if (strncmp(peek.ckID, "data", 4) == 0)
+			break;
+
+		fseek(f, beginningOfChunk + peek.cksize + 8, SEEK_SET);
 	}
-	else
-	{
-		fseek(f, beginning_of_WAVE + WAVE.c.cksize + 8, SEEK_SET);
-	}
-	
-	fread(&peek, 1, sizeof(peek) ,f);
-	
-	if (strncmp(peek.ckID, "data", 4) != 0)
-	{
-		fclose(f);
-		return false;
-	}
-	
+	while (strncmp(peek.ckID, "data", 4) != 0);
+
 	mLength = peek.cksize / (WAVE.wBitsPerSample / 8) / WAVE.nChannels;
 	mSampleRate = WAVE.nSamplesPerSec;
-	
+
 	int channels = WAVE.nChannels;
 	int bitsPerSample = WAVE.wBitsPerSample;
-	
+
 	mSamples = new int[mLength];
-	
+
 	const int bufferSize = 65536;
-	
+
 	short *buffer = new short[bufferSize * channels];
 	int totalRead = 0;
-	
+
 	while (totalRead < mLength)
 	{
 		int readCount = fread(buffer, (bitsPerSample / 8 * channels), bufferSize, f);
-		
+
 		for (int i = 0 ; i < readCount ; ++i)
-		{	
+		{
 			if (channels == 1)
 			{
 				mSamples[totalRead + i] = buffer[i * channels];
@@ -122,18 +121,18 @@ bool Wave::load(const char *filename)
 				mSamples[totalRead + i] = buffer[i * channels] + buffer[i * channels + 1];
 			}
 		}
-		
+
 		totalRead += readCount;
 	}
-	
+
 	delete [] buffer;
-	
+
 	fclose(f);
-	
+
 	return true;
 }
 
-	
+
 int Wave::getLength() const
 {
 	return mLength;
@@ -151,6 +150,6 @@ int Wave::get(int samplePosition) const
 {
 	if (samplePosition >= mLength || samplePosition < 0)
 		return 0;
-	
+
 	return mSamples[samplePosition];
 }
